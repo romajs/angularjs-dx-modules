85b5635aef2fa947e6dcc96daf7b4840e2ba5cd8
romajs <joseromualdoneto@gmail.com>
2014-08-23 21:37:36 -0300

    several improvements

--- angularjs/dextra.growl.notifications.js
+++ angularjs/dextra.growl.notifications.js
@@ -1,5 +1,101 @@
 var app = angular.module('dextra.growl.notifications', ['ngAnimate']);
 
+var GrownNotificationService = function(provider, dxI18n) {
+
+    this.add = function(level, value) {
+
+        if (typeof (value) === 'string') {
+            value = [value];
+        }
+
+        for (var i in value) {
+            provider.add(level, dxI18n(value[i]));
+        }
+
+    };
+
+    this.error = function(value) {
+        this.add('danger', value);
+    };
+
+    this.info = function(value) {
+        this.add('info', value);
+    };
+
+    this.success = function(value) {
+        this.add('success', value);
+    };
+
+    this.warn = function(value) {
+        this.add('warning', value);
+    };
+};
+
+var GrowlNotificationFactory = function(config, $timeout) {
+    
+    // TODO: transfer to controller
+
+    var self = this;
+    this.delay_ttl = config.delay_ttl;
+    this.count = 0;
+    this.messages = {};
+
+    this.add = function(type, value) {
+
+        this.registerAdd(type, value);
+        this.incDelay();
+    };
+
+    this.push = function(message) {
+
+        message.id = self.getId();
+        message.remove = function() {
+            // TODO:
+        };
+        return this.messages[message.id] = message;
+    };
+
+    this.getId = function() {
+
+        var id = new Date().getTime();
+        while (this.messages[id++] !== undefined)
+            ;
+        return id;
+    };
+
+    this.registerAdd = function(type, value) {
+
+        $timeout(function() {
+
+            var message = self.push({
+                type: type,
+                value: value,
+            });
+
+            self.registerRemove(message.id);
+
+        }, this.delay_ttl);
+    };
+
+    this.registerRemove = function(id) {
+
+        $timeout(function() {
+            delete self.messages[id];
+        }, config.fall_ttl);
+    };
+
+    this.incDelay = function() {
+        this.delay_ttl += config.delay_ttl;
+    };
+
+    this.decDelay = function() {
+        if (this.delay_ttl > 0) {
+            this.delay_ttl -= config.delay_ttl;
+        }
+    };
+
+};
+
 app.directive('dxGrowlNotifications', function() {
     return {
         scope: {
@@ -9,9 +105,12 @@ app.directive('dxGrowlNotifications', function() {
         controller: ['$scope', 'dxGrowlProvider', function($scope, provider) {
                 $scope.messages = provider.messages;
 
-                $scope.$watchCollection('messages', function(value) {
-                    console.info(value);
-                    provider.show_ttl -= 160;
+                $scope.$watchCollection('messages', function($new, $old) {
+//                    console.info('value.length: ' + $new.length);
+//                    console.info('value.length: ' + $old.length);
+                    if ($new.length > $old.length) {
+                        provider.decDelay();
+                    }
                 });
             }
         ],
@@ -19,7 +118,8 @@ app.directive('dxGrowlNotifications', function() {
             var template = [];
             template.push('<div class="dx-growl alert {{\'alert-\' + message.type}} {{class}}"');
             template.push(' ng-repeat="message in messages" role="alert">');
-            template.push('<button type="button" class="close" data-dismiss="alert" ng-if="dismissible">');
+            template.push('<button type="button" class="close" data-dismiss="alert"');
+            template.push(' ng-if="dismissible" ng-click="message.remove()">');
             template.push('<span aria-hidden="true">&times;</span>');
             template.push('</button>');
             template.push('<span>{{message.value}}</span>');
@@ -29,92 +129,16 @@ app.directive('dxGrowlNotifications', function() {
     };
 });
 
-var dxGrowlFactory = function(config, $timeout, dxI18n) {
-
-    var self = this;
-    this.count = 0;
-    this.messages = [];
-    this.queue = [];
-    this.show_ttl = config.show_ttl;
-
-    this.addQueue = function(type, value) {
-        $timeout(function() {
-            var index = self.messages.push({
-                id: this.count++,
-                type: type,
-                value: dxI18n(value)
-            });
-            $timeout(function() {
-                self.messages.splice(0, 1);
-            }, config.remove_ttl);
-        }, this.show_ttl);
-        console.info('this.show_ttl: ' + this.show_ttl);
-        this.show_ttl += config.show_ttl;
-//        return this.queue.push({
-//            id: this.count++,
-//            type: type,
-//            value: dxI18n(value)
-//        });
-    };
-
-    this.unQueue = function(index) {
-        this.addMessage(this.queue[index]);
-        this.queue.splice(index, 1);
-        if (this.queue.length > 0) {
-            $timeout(function() {
-                self.unQueue(0);
-            }, config.show_ttl);
-        }
-    };
-
-    this.addMessage = function(obj) {
-        this.messages.push(obj);
-        $timeout(function() {
-            self.messages.splice(0, 1);
-        }, config.remove_ttl);
-    };
-};
-
 app.provider('dxGrowlProvider', function() {
 
     var config = {
-        show_ttl: 160,
-        remove_ttl: 10000,
+        delay_ttl: 180,
+        fall_ttl: 5000,
     };
 
     this.$get = ['$timeout', 'dxI18n', function($timeout, dxI18n) {
-            return new dxGrowlFactory(config, $timeout, dxI18n);
+            return new GrowlNotificationFactory(config, $timeout, dxI18n);
         }];
 });
 
-app.service('dxGrowl', ['dxGrowlProvider', function(provider) {
-
-        this.add = function(level, value) {
-            if (typeof (value) === 'string') {
-                value = [value];
-            }
-            for (var i in value) {
-                provider.addQueue(level, value[i]);
-            }
-//            if (provider.queue && provider.queue.length > 0) {
-//                provider.unQueue(0);
-//            }
-        };
-
-        this.error = function(value) {
-            this.add('danger', value);
-        };
-
-        this.info = function(value) {
-            this.add('info', value);
-        };
-
-        this.success = function(value) {
-            this.add('success', value);
-        };
-
-        this.warn = function(value) {
-            this.add('warning', value);
-        };
-    }
-]);
+app.service('dxGrowl', ['dxGrowlProvider', 'dxI18n', GrownNotificationService]);
