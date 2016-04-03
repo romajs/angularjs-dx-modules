8106dda733543190509b29db1fa60c1d05760319
romajs <joseromualdoneto@gmail.com>
2016-04-03 19:27:42 -0300

    x

--- pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-
-CURRENT_DIR="$(dirname $(readlink -f $0))/"
-GIT_REPO_URL="git@github.com:romajs/angularjs-dx-modules.git" #"$(git --git-dir=$DIR.git config --get remote.origin.url)"
-git subtree pull --prefix=$CURRENT_DIR $GIT_REPO_URL master --squash
