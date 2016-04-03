d069aaeb5fedc68dd0fb8ca7dc28984e15aab586
romajs <joseromualdoneto@gmail.com>
2016-04-03 19:17:32 -0300

    pull.sh & push.sh

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -xe
+
+CURRENT_DIR="$(dirname $(readlink -f $0))/"
+GIT_REPO_URL="$(git --git-dir=$DIR.git config --get remote.origin.url)"
+git subtree push --prefix=$CURRENT_DIR git@$GIT_REPO_URL master --squash
